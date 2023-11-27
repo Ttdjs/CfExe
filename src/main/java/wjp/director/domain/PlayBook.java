ce7e34cae27fd04f0bb0d2b7ff13e35b85d06380
lingse <1725154452@qq.com>
2023-11-28 01:09:00 +0800

    director

--- src/main/java/wjp/director/domain/PlayBook.java
+++ src/main/java/wjp/director/domain/PlayBook.java
@@ -3,9 +3,7 @@ package wjp.director.domain;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author lingse
@@ -14,16 +12,32 @@ import java.util.Map;
 public class PlayBook {
     private String apiName;
     // 依赖关系
-    private Map<ExecuteTask, List<ExecuteTask>> dependencys;
+    private Map<ExecuteTask, List<ExecuteTask>> dependencys = new HashMap<>();
     // todo 不可变
     @Getter
-    private List<ExecuteTask> aggreTaskDepedency;
+    private List<ExecuteTask> aggreTaskDepedency = new ArrayList<>();
     // 执行顺序
-    private List<ExecuteTask> orders;
+    private List<ExecuteTask> orders = new ArrayList<>();
     @Getter
     private AggreTask aggreTask;
     public List<ExecuteTask> queryDependencyByTask(ExecuteTask executeTask) {
         return dependencys.getOrDefault(executeTask, Collections.emptyList());
     }
+    public void setDependencysByTask(ExecuteTask task, ExecuteTask... dependency) {
+        if (this.dependencys == null) {
+            dependencys = new HashMap<>();
+        }
+        if (dependencys.containsKey(task)) {
+            // todo 自定义异常
+            throw new RuntimeException("重复添加" + task.getClass().getSimpleName());
+        }
+        dependencys.put(task, Arrays.asList(dependency));
+    }
 
+    public void setAggreTaskDepedency(List<ExecuteTask> aggreTaskDepedency) {
+        if (!aggreTaskDepedency.isEmpty()) {
+            throw new RuntimeException("重复添加聚合任务依赖" + apiName);
+        }
+        this.aggreTaskDepedency = aggreTaskDepedency;
+    }
 }
